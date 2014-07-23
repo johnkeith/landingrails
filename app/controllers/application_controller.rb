bed337df405f1aaaa7d65531348df773e1dab7d6
John Keith <johnkeith@outlook.com>
2014-07-23 17:29:47 -0400

    after filter for discard flash on app.contr., reload flash method in app helper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,17 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  after_filter :discard_flash_if_ajax
 
   def registrations_accepted
     if User.count != 0
       redirect_to root_path, alert: "Accounts cannot be created at this time."
     end
+  end
+
+  private
+  
+  def discard_flash_if_ajax
+    flash.discard if request.xhr?
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def reload_flash
+    page.replace_html "flash_messages", partial: 'partials/flashes'
+  end
 end
